f88eea39d590b43c22a2eb615bfd251883461e28
mahnaf <mahnaf125@gmail.com>
2025-04-26 01:13:21 +0600

    Initialized Project
    Setup Project, Installed Tailwind CSS and React Router, Set Favicon and Title, Set Primary Color and Font, Created Root Route.

---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <>
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </>
+    );
+};
+
+export default Root;
---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router';
+import Root from './pages/Root/Root';
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root
+    }
+])
+
+export default router
